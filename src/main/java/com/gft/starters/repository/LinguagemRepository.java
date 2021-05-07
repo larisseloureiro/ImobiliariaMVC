61e73b82aebdef21750ef6a39b87ced18f0c8444
Larisse Loureiro <larisseloureirob@gmail.com>
2021-05-07 11:20:03 -0300

    att

--- src/main/java/com/gft/starters/repository/LinguagemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gft.starters.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gft.starters.model.Linguagem;
-
-public interface LinguagemRepository extends JpaRepository<Linguagem, Long> {
-
-}
--- src/main/java/com/gft/starters/repository/StarterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gft.starters.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gft.starters.model.Starter;
-
-public interface StarterRepository extends JpaRepository<Starter, Long> {
-
-}
